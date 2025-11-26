cd7c0672af586edd2d5f0694b55c2ab116f599f7
wr1 <8971152+wr1@users.noreply.github.com>
2025-11-26 13:38:59 +0100

    Update import for B32dStep from statesman

--- src/b3_2d/__init__.py
+++ src/b3_2d/__init__.py
@@ -1,7 +1,7 @@
 """b3_2d: 2D meshing for b3m using cgfoil."""
 
 from .core.mesh import process_vtp_multi_section
-from .core.b3_2d_step import B32dStep
+from .statesman.b3_2d_step import B32dStep
 
 __version__ = "0.1.0"
 __all__ = ["process_vtp_multi_section", "B32dStep"]
